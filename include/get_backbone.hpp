25c3f0fc93be477df9c180403a776c56adf95a89
snljty <snljty@gmail.com>
2026-08-16 12:54:20 +0800

    fix typo in periodic table

--- include/get_backbone.hpp
+++ include/get_backbone.hpp
@@ -35,7 +35,7 @@ constexpr std::array<std::string_view, max_element_num + 1> element_names = {
     "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", 
     "Hf", "Ta", "W" , "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn",
     "Fr", "Ra", "Ac", "Th", "Pa", "U" , "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", 
-    "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nb", "Fl", "Mc", "Lv", "Ts", "Og"
+    "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og"
 };
 
 constexpr std::array<float, max_element_num + 1> element_covalence_radius = {
